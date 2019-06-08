5a503a7cad5ce1a7a6dd34412c3107ddfb056358
Gaurav Yadav <gaurav.yadav@webklipper.com>
2019-06-08 21:17:32 +0530

    adding empty project

---
+++ src/main/java/indorse/auth/wizard/authwizard/AuthWizardApplication.java
@@ -0,0 +1,13 @@
+package indorse.auth.wizard.authwizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthWizardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthWizardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/indorse/auth/wizard/authwizard/AuthWizardApplicationTests.java
@@ -0,0 +1,16 @@
+package indorse.auth.wizard.authwizard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuthWizardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
