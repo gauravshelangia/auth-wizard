2079a4f336015155eea2c15084e5f56d7e946cc9
Gaurav Yadav <gaurav.yadav@webklipper.com>
2019-06-08 21:28:36 +0530

    Added data source and User Model

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # auth-wizard
+
+This application contains simple user auth system with spring boot as multi-module project. 
+
+##Requirements
+1. ###Install postgresql 
+    * `brew install postgres` (Mac OsX command)
+    * `brew services start postgresql` (start postgres)
+2. ###Setting up user and database with postgres
+    * `create user root with encrypted password 'admin';` (create use)
+    * `create database auth_wizard; (create database);`
+    * `grant all privileges on database auth_wizard to root;` (grant permission to the user on database auth_wizard)
+        
+      
---
+++ src/main/java/indorse/auth/wizard/authwizard/Model/Users.java
@@ -0,0 +1,103 @@
+package indorse.auth.wizard.authwizard.Model;
+
+import org.springframework.data.annotation.Transient;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password")
+    @Transient
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "enabled")
+    private boolean enabled;
+
+    @Column(name = "confirmation_token", unique = true)
+    private String confirmationToken;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getConfirmationToken() {
+        return confirmationToken;
+    }
+
+    public void setConfirmationToken(String confirmationToken) {
+        this.confirmationToken = confirmationToken;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", enabled=" + enabled +
+                ", confirmationToken='" + confirmationToken + '\'' +
+                '}';
+    }
+}
